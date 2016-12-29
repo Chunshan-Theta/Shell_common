8d9d33a337a319cd0b3015b09162dd9ffdcfe2e5
Wang <gavin19950511@gmail.com>
2016-12-29 11:08:54 +0800

    Create HelloWorld.sh

---
+++ HelloWorld.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+# Program:
+#       This program shows "Hello World!" in your screen.
+# History:
+# 2015/07/16	VBird	First release
+#PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+#export PATH
+ls
+#python /var/www/html/CmdPHP/hello.py
+#touch a.txt
+echo "Hello World! \a \n"
+#sudo python
+
+#use password for sudo
+echo gavin | sudo -S python
+echo "\n"
+
+#end
+exit 0
