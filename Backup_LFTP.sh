bb984aa27940fb0b7c4ea2c3430365c1e8207cd3
Wang <gavin19950511@gmail.com>
2017-09-13 21:58:30 -0500

    Update and rename BackupAndSFTP.sh to Backup_LFTP.sh

---
+++ Backup_LFTP.sh
@@ -0,0 +1,32 @@
+cd /home/gavin/backup  #base dir for put file
+
+
+# back up Mysql
+mysqldump -A > all_databases.sql -u root -proot -h localhost
+# restore Mysql
+mysql <all_databases.sql -u root -proot
+
+
+# backup Web File
+# '--exclude' could skip dir you don't want to back up
+sudo tar cvpzf backup.tgz /var/www/
+
+#!/bin/sh
+HOST=140.115.126.216
+USER=gavin	
+PASSWD=gavin
+TargetDir=/home/gavin/Desktop/test #Target dir for put file
+
+
+
+lftp<<END_SCRIPT
+open sftp://$HOST
+user $USER $PASSWD
+cd $TargetDir
+put all_databases.sql
+put backup.tgz
+bye
+END_SCRIPT
+#end
+
+exit 0
