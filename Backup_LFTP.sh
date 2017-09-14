8dd1fc011702c1df9c59aa560c23493cfc8230ad
Wang <gavin19950511@gmail.com>
2017-09-14 14:10:41 +0800

    Update Backup_LFTP.sh

--- Backup_LFTP.sh
+++ Backup_LFTP.sh
@@ -1,32 +1,33 @@
-cd /home/gavin/backup  #base dir for put file
+#cd /home  #base dir for put file
 
+ID=20$(date +"%y_%m_%dv%I_%M_%S")
 
 # back up Mysql
-mysqldump -A > all_databases.sql -u root -proot -h localhost
+mysqldump -A > ${ID}all_databases.sql -u USER -pPASSWORD -h localhost
 # restore Mysql
-mysql <all_databases.sql -u root -proot
+#mysql <all_databases.sql -u root -pWulab35415
 
 
 # backup Web File
 # '--exclude' could skip dir you don't want to back up
-sudo tar cvpzf backup.tgz /var/www/
-
-#!/bin/sh
-HOST=140.115.126.216
-USER=gavin	
-PASSWD=gavin
-TargetDir=/home/gavin/Desktop/test #Target dir for put file
+sudo tar cvpzf ${ID}backup.tgz /var/www/
 
 
+HOST=IPADRESS
+USER=USER
+PASSWD=PASSWORD
+TargetDir=/home/BackupOfWuLAB/126_15 #Target dir for put file
+echo $ID
 
 lftp<<END_SCRIPT
+
 open sftp://$HOST
 set sftp:auto-confirm yes
 user $USER $PASSWD
 cd $TargetDir
-put all_databases.sql
-put backup.tgz
-bye
+put ${ID}all_databases.sql
+put ${ID}backup.tgz
+
 END_SCRIPT
 #end
 
