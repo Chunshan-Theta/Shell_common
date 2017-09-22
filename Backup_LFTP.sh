f461191bae1d07ec5d71182991af41475828e283
Wang <gavin19950511@gmail.com>
2017-09-22 10:33:15 +0800

    Update Backup_LFTP.sh

--- Backup_LFTP.sh
+++ Backup_LFTP.sh
@@ -1,9 +1,12 @@
+
+#備份部份
 #cd /home  #base dir for put file
 
 ID=20$(date +"%y_%m_%dv%I_%M_%S")
 
 # back up Mysql
-mysqldump -A > /home/.../${ID}all_databases.sql -u root -pWulab35415 -h localhost
+mysqldump -A > /home/.../${ID}all_databases.sql -u USERNAME -pPASSWORD -h localhost
+#-p需與密碼相連
 # restore Mysql
 #mysql <all_databases.sql -u root -pWulab35415
 
@@ -12,6 +15,7 @@ mysqldump -A > /home/.../${ID}all_databases.sql -u root -pWulab35415 -h localhos
 # '--exclude' could skip dir you don't want to back up
 echo "PassWord" | sudo -S tar cvpzf /home/.../${ID}backup.tgz /var/www/
 
+# 傳送部份
 
 HOST=140.....
 USER=.....	
